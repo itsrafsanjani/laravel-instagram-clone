65c2bdd2cf00a7e2786aa446f9a52d1fcab07b94
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-05-17 16:10:02 +0600

    Show Profile Page Img Src Tag Removed

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -45,8 +45,7 @@
             @forelse($user->posts as $post)
                 <div class="col-md-4 pb-4">
                     <a href="{{ route('posts.show', $post) }}">
-                        <img class="lazy w-100 rounded" data-src="{{ $post->image() }}" alt="{{ $post->title }}"
-                             src="{{ $post->caption }}">
+                        <img class="lazy w-100 rounded" data-src="{{ $post->image() }}" alt="{{ $post->title }}">
                     </a>
                 </div>
             @empty
