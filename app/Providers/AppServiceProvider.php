bb707e4244910ae5a78250e5609420918492a3bf
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-05-30 13:56:28 +0600

    feat: upgrade to laravel 7.x to 8.x

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
         if(config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
+
+        Paginator::useBootstrap();
     }
 }
