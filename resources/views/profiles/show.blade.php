28f69f52b5e3663a0443a3a2a2b2e3f6e9982389
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-05-30 14:02:24 +0600

    fix: profile website url link fixed

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -37,7 +37,7 @@
                 </div>
                 <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
                 <div>{{ $user->profile->description }}</div>
-                <div><a href="#">{{ $user->profile->url }}</a></div>
+                <div><a href="{{ $user->profile->url }}" target="_blank">{{ $user->profile->url }}</a></div>
             </div>
         </div>
 
