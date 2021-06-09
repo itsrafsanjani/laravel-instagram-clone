9ad486a1f6cbfe5c7bbd16b2b76c72b82f52762e
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-06-10 01:55:24 +0600

    feat: meta description added with meta image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Search Engine -->
+    <meta name="description" content="Laragram is an open source social media platform created with Laravel and VueJS.">
+    <meta name="image" content="https://i.ibb.co/R71RdXF/laragram.jpg">
+    <!-- Schema.org for Google -->
+    <meta itemprop="name" content="Laragram | Social Media">
+    <meta itemprop="description" content="Laragram is an open source social media platform created with Laravel and VueJS.">
+    <meta itemprop="image" content="https://i.ibb.co/R71RdXF/laragram.jpg">
+    <!-- Open Graph general (Facebook, Pinterest & Google+) -->
+    <meta name="og:title" content="Laragram | Social Media">
+    <meta name="og:description" content="Laragram is an open source social media platform created with Laravel and VueJS.">
+    <meta name="og:image" content="https://i.ibb.co/R71RdXF/laragram.jpg">
+    <meta name="og:locale" content="en_US, bn_BD">
+    <meta name="og:type" content="website">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
