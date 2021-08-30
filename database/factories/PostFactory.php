939365b4eda8cbf17bf962fe35ba6704d4a3ebc4
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-08-30 08:49:14 +0600

    feat: PostFactory and UserFactory updated

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,17 +1,33 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\Post;
 use App\User;
-use Illuminate\Support\Str;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Str;
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'user_id' => User::all()->random()->id,
-        'slug' => Str::random(12),
-        'caption' => 'My first upload..',
-        'image' => '/storage/uploads/image.jpg'
-    ];
-});
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'slug' => Str::random(12),
+            'caption' => 'My first upload..',
+            'image' => '/storage/uploads/image.jpg'
+        ];
+    }
+}
