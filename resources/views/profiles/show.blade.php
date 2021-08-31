8c65f64109f924627562e6d3e105f8fe723cfbfa
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-08-31 23:59:15 +0600

    feat: modal added in profiles.show page

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -64,10 +64,32 @@
         <div class="row pt-5 mx-auto">
             @forelse($user->posts as $post)
                 <div class="col-md-4 pb-4">
-                    <a href="{{ route('posts.show', $post) }}">
-                        <img class="lazy w-100 rounded" src="{{ asset('images/placeholder.jpg') }}"
+                        <img type="button" class="lazy w-100 rounded" src="{{ asset('images/placeholder.jpg') }}"
+                             data-src="{{ $post->image() }}" alt="{{ $post->title }}" data-toggle="modal" data-target="#post{{ $post->id }}">
+
+                    {{-- Modal --}}
+                    <div class="modal fade" id="post{{ $post->id }}" tabindex="-1" aria-labelledby="post{{ $post->id }}Label" aria-hidden="true">
+                        <div class="modal-dialog modal-lg modal-dialog-centered">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="post{{ $post->id }}Label">{{ $post->caption }}</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <img class="lazy w-100 rounded" src="{{ asset('images/placeholder.jpg') }}"
                              data-src="{{ $post->image() }}" alt="{{ $post->title }}">
-                    </a>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary" onclick="window.open('{{ route('posts.show', $post) }}')">
+                                    Details
+                                </button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             @empty
                 <div class="col-md-6 mx-auto">
