a43df3d15f899f79d9353349958cc624b4f044d2
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-01 18:44:52 +0600

    feat: footer responsive updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,15 +109,15 @@
 
     <footer class="footer bg-glass mt-auto py-2">
         <div class="container">
-            <div class="d-flex justify-content-between">
-                    <span class="text-muted"><i class="far fa-copyright"></i>
+            <div class="d-md-flex text-center justify-content-between">
+                    <div class="text-muted"><i class="far fa-copyright"></i>
                         <a href="{{ config('app.url') }}" target="_blank">{{ config('app.name') }}</a>
-                    </span>
-                <span class="text-muted">
+                    </div>
+                <div class="text-muted">
                     <a href="{{ route('static-pages.privacy-policy') }}" target="_blank" class="ml-2">Privacy Policy</a>
                     <a href="{{ route('static-pages.terms-of-service') }}" target="_blank" class="ml-2">Terms of Service</a>
                     <a href="{{ route('static-pages.cookies') }}" target="_blank" class="ml-2">Cookies</a>
-                </span>
+                </div>
             </div>
         </div>
     </footer>
