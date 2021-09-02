b607f211c00626266e96296e86f3405ad06da567
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-02 10:56:34 +0600

    feat: profiles.show page design updated

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -9,9 +9,11 @@
                     class="rounded-circle w-100" alt="{{ $user->username }}">
             </div>
             <div class="col-md-9 pt-5">
-                <div class="d-flex justify-content-between align-items-baseline mb-2">
-                    <div class="d-flex align-items-center">
-                        <div class="h4">{{ $user->username }}</div>
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <div class="d-flex">
+                        <div>
+                            <div class="text-lg font-weight-bold">{{ $user->username }}</div>
+                        </div>
 
                         @if(auth()->user()->username != $user->username)
                             {{-- <follow-button username="{{ $user->username }}" follows="{{ $follows }}"></follow-button> --}}
@@ -20,23 +22,25 @@
                                 {{ $follows ? 'Unfollow' : 'Follow' }}
                             </button>
                         @endif
+
+                        @can('update', $user->profile)
+                            <div class="ml-4">
+                                <a href="{{ route('profiles.edit', $user) }}" class="btn btn-sm btn-secondary">Edit Profile</a>
+                            </div>
+                        @endcan
                     </div>
 
                     @can('update', $user->profile)
-                        <a href="{{ route('posts.create') }}" class="btn btn-sm btn-secondary">Add New Post</a>
+                        <div>
+                            <a href="{{ route('posts.create') }}" class="btn btn-sm btn-secondary">Add New Post</a>
+                        </div>
                     @endcan
 
                 </div>
 
-                @can('update', $user->profile)
-                    <div class="my-2">
-                        <a href="{{ route('profiles.edit', $user) }}" class="btn btn-sm btn-secondary">Edit Profile</a>
-                    </div>
-                @endcan
-
-                <div class="d-flex">
-                    <div class="pr-5 text-center"><strong>{{ $postCount }}</strong> posts</div>
-                    <div class="pr-5 text-center">
+                <div class="d-flex mb-3">
+                    <div class="text-center"><strong>{{ $postCount }}</strong> posts</div>
+                    <div class="pl-5 text-center">
                         @if($followersCount > 0)
                             <a href="{{ route('profiles.followers', $user->username) }}">
                                 <strong id="followersCount">{{ $followersCount }}</strong> followers
@@ -45,7 +49,7 @@
                             <strong id="followersCount">{{ $followersCount }}</strong> followers
                         @endif
                     </div>
-                    <div class="pr-5 text-center">
+                    <div class="pl-5 text-center">
                         @if($followingCount > 0)
                             <a href="{{ route('profiles.followings', $user->username) }}">
                                 <strong id="followingCount">{{ $followingCount }}</strong> following
@@ -55,9 +59,17 @@
                         @endif
                     </div>
                 </div>
-                <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
-                <div>{{ $user->profile->description }}</div>
-                <div><a href="{{ $user->profile->url }}" target="_blank">{{ $user->profile->url }}</a></div>
+                <div class="pt-4">
+                    @if ($user->name)
+                        <div class="font-weight-bold">{{ $user->name }}</div>
+                    @endif
+                    @if ($user->profile->description)
+                        <div>{{ $user->profile->description }}</div>
+                    @endif
+                    @if ($user->profile->url)
+                        <div><a href="{{ $user->profile->url }}" target="_blank">{{ $user->profile->url }}</a></div>
+                    @endif
+                </div>
             </div>
         </div>
 
