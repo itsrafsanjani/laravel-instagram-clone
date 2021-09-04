6995dd06d703fe143547930098d50cd6847968cb
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-04 17:36:58 +0600

    feat: home button added in messages page nav

--- resources/views/vendor/Chatify/pages/app.blade.php
+++ resources/views/vendor/Chatify/pages/app.blade.php
@@ -8,6 +8,7 @@
                 <a href="#"><i class="fas fa-inbox"></i> <span class="messenger-headTitle">MESSAGES</span> </a>
                 {{-- header buttons --}}
                 <nav class="m-header-right">
+                    <a href="/"><i class="fas fa-home"></i></a>
                     <a href="#"><i class="fas fa-cog settings-btn"></i></a>
                     <a href="#" class="listView-x"><i class="fas fa-times"></i></a>
                 </nav>
