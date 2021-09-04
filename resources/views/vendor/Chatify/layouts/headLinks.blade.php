cbd5155e3a16688de982cb5dff9f1bd8e34cec59
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-04 17:10:19 +0600

    feat: tailwindcss added, app.js removed from chatify layout, chatify style.css updated

--- resources/views/vendor/Chatify/layouts/headLinks.blade.php
+++ resources/views/vendor/Chatify/layouts/headLinks.blade.php
@@ -8,17 +8,17 @@
 
 {{-- scripts --}}
 <script
-  src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  src="//code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="{{ asset('js/chatify/font.awesome.min.js') }}"></script>
 <script src="{{ asset('js/chatify/autosize.js') }}"></script>
-<script src="{{ asset('js/app.js') }}"></script>
-<script src='https://unpkg.com/nprogress@0.2.0/nprogress.js'></script>
+{{--<script src="{{ asset('js/app.js') }}"></script>--}}
+<script src='//unpkg.com/nprogress@0.2.0/nprogress.js'></script>
 
 {{-- styles --}}
-<link rel='stylesheet' href='https://unpkg.com/nprogress@0.2.0/nprogress.css'/>
+<link rel='stylesheet' href='//unpkg.com/nprogress@0.2.0/nprogress.css'/>
 <link href="{{ asset('css/chatify/style.css') }}" rel="stylesheet" />
 <link href="{{ asset('css/chatify/'.$dark_mode.'.mode.css') }}" rel="stylesheet" />
-<link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+<link href="//unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
 {{-- Messenger Color Style--}}
 @include('Chatify::layouts.messengerColor')
