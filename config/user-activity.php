a57078a5a348f7d42f6ccb72edeae0554ef2c270
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-05 12:51:09 +0600

    feat: user activity added
    visit '/admin/user-activity' to view

---
+++ config/user-activity.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'activated'        => true, // active/inactive all logging
+    'middleware'       => ['web', 'auth'],
+    'route_path'       => 'admin/user-activity',
+    'admin_panel_path' => 'admin/dashboard',
+    'delete_limit'     => 7, // default 7 days
+
+    'model' => [
+        'user' => "App\Models\User"
+    ],
+
+    'log_events' => [
+        'on_create'     => false,
+        'on_edit'       => true,
+        'on_delete'     => true,
+        'on_login'      => true,
+        'on_lockout'    => true
+    ]
+];
