21c108dcef4a6418f1ea814b249aa9cdb29553cb
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-06 23:18:24 +0600

    feat: prevent lazy loading models added in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        Model::preventLazyLoading(!$this->app->isProduction());
     }
 }
