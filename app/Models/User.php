591f5049bb14d6ceace286462ebad677d887684a
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-07 23:02:05 +0600

    feat: User model fillable and casts updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use BeyondCode\Comments\Contracts\Commentator;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Overtrue\LaravelFollow\Followable;
 use Staudenmeir\EloquentEagerLimit\HasEagerLimit;
@@ -15,29 +14,23 @@ class User extends \TCG\Voyager\Models\User implements MustVerifyEmail, Commenta
     use HasFactory, Notifiable, HasEagerLimit, Followable;
 
     const PAGINATE_COUNT = 20;
-
-    /**
-     * Check if a comment for a specific model needs to be approved.
-     * @param mixed $model
-     * @return bool
-     */
-    public function needsCommentApproval($model): bool
-    {
-        return false;
-    }
-
-    public function getRouteKeyName()
-    {
-        return 'username';
-    }
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'username', 'password',
+        'name',
+        'username',
+        'password',
+        'email',
+        'phone_number',
+        'phone_number_verified_at',
+        'otp',
+        'otp_created_at',
+        'website',
+        'bio',
+        'gender',
     ];
 
     /**
@@ -56,6 +49,8 @@ class User extends \TCG\Voyager\Models\User implements MustVerifyEmail, Commenta
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'phone_number_verified_at' => 'datetime',
+        'otp_created_at' => 'datetime'
     ];
 
     protected static function boot()
@@ -69,6 +64,21 @@ class User extends \TCG\Voyager\Models\User implements MustVerifyEmail, Commenta
 
 //            Mail::to($user->email)->send(new NewUserWelcomeMail());
         });
+    }
+
+    /**
+     * Check if a comment for a specific model needs to be approved.
+     * @param mixed $model
+     * @return bool
+     */
+    public function needsCommentApproval($model): bool
+    {
+        return false;
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'username';
     }
 
     public function posts()
