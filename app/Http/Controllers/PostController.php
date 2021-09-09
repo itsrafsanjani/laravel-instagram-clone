1e467bd42a42d5b319e522e6a2de0e23c5a37bdb
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-10 00:57:07 +0600

    feat: PostController@index method fixed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $users = auth()->user()->followings;
+        $users = auth()->user()->followings()->pluck('users.id');
 
         $posts = Post::with([
             'user', 'likes', 'user.likes', 'comments' => function ($query) {
