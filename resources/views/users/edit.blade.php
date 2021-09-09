fa5d4f3266b9287fbdaea5e532d2d6de831f9461
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-09 12:58:12 +0600

    feat: users.edit page updated

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,6 +8,21 @@
             <div class="row">
                 <div class="col-md-8 mx-auto">
                     <div class="card">
+                        <!-- Card body -->
+                        <div class="card-body">
+                            <img src="{{ $user->avatar }}"
+                                 class="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                                 alt="{{ $user->name }}">
+                            <div class="pt-4 text-center">
+                                <h5 class="h3 title">
+                                    <span class="d-block mb-1">{{ $user->name }}</span>
+                                    <small class="h4 font-weight-light text-muted">{{ $user->bio }}</small>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card">
                         <div class="card-header d-flex justify-content-between align-items-center">
                             <h3 class="mb-0">Edit Profile</h3>
                         </div>
@@ -15,55 +30,149 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group">
-                                        <label for="description">Description</label>
+                                        <label for="name">Name</label>
 
-                                        <input id="description"
+                                        <input id="name"
                                                type="text"
-                                               class="form-control {{ $errors->has('description') ? ' is-invalid' : '' }}"
-                                               name="description"
-                                               value="{{ old('description') ?? $user->profile->description }}"
-                                               autocomplete="description" autofocus>
+                                               class="form-control @error('name') is-invalid @enderror"
+                                               name="name"
+                                               value="{{ old('name') ?? $user->name }}"
+                                               autocomplete="name">
 
-                                        @if ($errors->has('description'))
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('description') }}</strong>
-                                            </span>
-                                        @endif
+                                        @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="url">URL</label>
+                                        <label for="username">Username</label>
 
-                                        <input id="url"
+                                        <input id="username"
                                                type="text"
-                                               class="form-control {{ $errors->has('url') ? ' is-invalid' : '' }}"
-                                               name="url"
-                                               value="{{ old('url') ?? $user->profile->url }}"
-                                               autocomplete="url" autofocus>
+                                               class="form-control @error('username') is-invalid @enderror"
+                                               name="username"
+                                               value="{{ old('username') ?? $user->username }}"
+                                               autocomplete="username">
 
-                                        @if ($errors->has('url'))
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('url') }}</strong>
-                                            </span>
-                                        @endif
+                                        @error('username')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="image">Profile Image</label>
+                                        <label for="email">Email</label>
 
-                                        <input type="file" class="form-control-file" id="image" name="image" oninput="document.getElementById('pic').src=window.URL.createObjectURL(this.files[0])">
+                                        <input id="email"
+                                               type="email"
+                                               class="form-control @error('email') is-invalid @enderror"
+                                               name="email"
+                                               value="{{ old('email') ?? $user->email }}"
+                                               autocomplete="email">
 
-                                        <img src="{{ auth()->user()->avatar }}" id="pic" class="img-thumbnail my-3" alt=""/>
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="phone_number">Phone Number</label>
+
+                                        <input id="phone_number"
+                                               type="tel"
+                                               class="form-control @error('phone_number') is-invalid @enderror"
+                                               name="phone_number"
+                                               value="{{ old('phone_number') ?? $user->phone_number }}"
+                                               autocomplete="phone_number">
+
+                                        @error('phone_number')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="website">Website</label>
+
+                                        <input id="website"
+                                               type="url"
+                                               class="form-control @error('website') is-invalid @enderror"
+                                               name="website"
+                                               value="{{ old('website') ?? $user->website }}"
+                                               autocomplete="website">
+
+                                        @error('website')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="bio">Bio</label>
+
+                                        <textarea id="bio"
+                                                  type="text"
+                                                  class="form-control @error('bio') is-invalid @enderror"
+                                                  name="bio"
+                                                  autocomplete="bio">{{ old('bio') ?? $user->bio }}</textarea>
+
+                                        @error('bio')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="gender">Gender</label>
+
+                                        <select id="gender"
+                                                class="form-control @error('gender') is-invalid @enderror"
+                                                name="gender"
+                                                autocomplete="gender">
+                                            <option value="{{ old('gender') ?? $user->gender }}">
+                                                {{ Str::ucfirst($user->gender) }}
+                                            </option>
+                                            <option value="male">Male</option>
+                                            <option value="female">Female</option>
+                                            <option value="others">Others</option>
+                                        </select>
+
+                                        @error('gender')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="avatar">Avatar</label>
+
+                                        <input type="file" class="form-control-file" id="avatar" name="avatar"
+                                               oninput="document.getElementById('avatar-img').src=window.URL.createObjectURL(this.files[0])">
+
+                                        <img src="{{ $user->avatar }}" id="avatar-img" class="img-thumbnail my-3"
+                                             alt="{{ $user->name }}"/>
 
                                         <div class="text-muted">
                                             <small>
-                                                Choose a square image for best experience! <strong><a href="{{ route('notices.image') }}">Rules!</a></strong>
+                                                Choose a square image for best experience! <strong><a
+                                                        href="{{ route('notices.image') }}">Rules!</a></strong>
                                             </small>
                                         </div>
 
-                                        @if ($errors->has('image'))
-                                            <strong>{{ $errors->first('image') }}</strong>
-                                        @endif
+                                        @error('avatar')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
                                     </div>
 
                                     <div class="form-group pt-4">
