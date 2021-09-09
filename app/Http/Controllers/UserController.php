63192e0ba835d256d050cec170f41fab04993cb6
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-09 18:09:39 +0600

    feat: UpdateUserRequest and UserController@update updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\Profile;
 use App\Models\User;
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
@@ -40,61 +41,21 @@ class UserController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(User $user, Request $request)
+    public function update(User $user, UpdateUserRequest $request)
     {
         $this->authorize('update', $user);
 
-        $data = request()->validate([
-            'description' => 'required',
-            'url' => 'url',
-            'image' => '',
-        ]);
-
-        if (request('image')) {
-            //            $imagePath = request('image')->store('profile', 'public');
-            //
-            //            $image = Image::make(public_path("/storage/{$imagePath}"))->fit(1080, 1080, function ($constraint) {
-            //                $constraint->upsize();
-            //            });
-
-            $imagePath = Cloudinary::upload($request->file('image')->getRealPath(), [
-                'folder' => 'laragram/avatar',
-                'transformation' => [
-                    'background' => 'white',
-                    'width' => 400,
-                    'height' => 400,
-                    'crop' => 'pad'
-                ]
-            ])->getSecurePath();
-
-            $client = new SightengineClient(config('services.sightengine.user'), config('services.sightengine.secret'));
-            $output = $client->check(['nudity'])->set_url($imagePath);
-
-            if ($output->nudity->safe > 0.5 && $output->nudity->raw < 0.1) {
-
-                $imageArray = ['image' => $imagePath];
-                auth()->user()->profile->update(array_merge(
-                    $data,
-                    $imageArray ?? []
-                ));
-
-                return redirect()->route('users.show', $user)->with([
-                    'status' => 'success',
-                    'message' => 'Profile uploaded successfully!'
-                ]);
-            }
-
-            return redirect()->route('users.show', $user)->with([
-                'status' => 'error',
-                'message' => 'Your image contains inappropriate content!'
-            ]);
+        if (! empty($request->password)) {
+            $request->merge(['password' => bcrypt($request->password)]);
         }
 
-        auth()->user()->profile->update($data);
+        $user->update($request->validated() + [
+                $request->password
+            ]);
 
         return redirect()->route('users.show', $user)->with([
             'status' => 'success',
-            'message' => 'Profile uploaded successfully!'
+            'message' => 'Profile updated successfully!'
         ]);
     }
 
---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['sometimes'],
+            'username' => ['sometimes'],
+            'password' => ['sometimes'],
+            'email' => ['sometimes', 'email'],
+            'phone_number' => ['sometimes'],
+            'phone_number_verified_at' => ['sometimes'],
+            'otp' => ['sometimes'],
+            'otp_created_at' => ['sometimes'],
+            'website' => ['sometimes', 'url'],
+            'bio' => ['sometimes', 'max:150'],
+            'gender' => ['sometimes'],
+            'avatar' => ['sometimes'],
+        ];
+    }
+}
