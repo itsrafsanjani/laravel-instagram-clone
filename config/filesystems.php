81f4bdae8e3820c8312d462c41934c3975260747
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-09-23 21:01:20 +0600

    feat: cloudinary config added to filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,33 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
+        'cloudinary' => [
+            'driver'         => 'cloudinary',
+            'api_key'        => env('CLOUDINARY_API_KEY'),
+            'api_secret'     => env('CLOUDINARY_API_SECRET'),
+            'cloud_name'     => env('CLOUDINARY_CLOUD_NAME'),
+            'secure'         => env('CLOUDINARY_SECURE', true),
+            'resource_types' => [
+                'image' => [
+                    'png',
+                    'jpeg',
+                    'jpg',
+                ],
+                'video' => [
+                    'mp4',
+                    'avi',
+                    'mp3',
+                    'flac',
+                ],
+                'raw'   => [
+                    'pdf',
+                    'xlsx',
+                    'csv',
+                    'txt',
+                ],
+            ],
+        ],
+
     ],
 
     /*
