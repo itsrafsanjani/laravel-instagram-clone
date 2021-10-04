1fa9d2b1d15dc068b657396ca4ec82107f4973b9
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-10-04 16:53:38 +0600

    feat: search bar is only available if user is authenticated

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -13,10 +13,12 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-                {{-- Search bar --}}
-                <form class="form-inline my-2 my-md-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                </form>
+                @auth
+                    {{-- Search bar --}}
+                    <form class="form-inline my-2 my-md-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    </form>
+                @endauth
             </ul>
 
             <!-- Right Side Of Navbar -->
