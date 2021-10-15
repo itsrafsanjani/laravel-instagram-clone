d17cede66579d22201447dc4b2743e937306c188
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-10-15 16:16:57 +0600

    fix: UserFactory role_id removed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,6 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'role_id' => 2,
             'name' => $this->faker->name,
             'username' => $this->faker->unique()->userName,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
