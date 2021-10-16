ae7440973243287c05e4ea756b9b85a33b2c0237
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-10-16 13:48:52 +0600

    fix: in login, register view required attribute added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,10 +17,14 @@
                                 <div class="col-md-6">
                                     <input id="name" type="text"
                                            class="form-control @error('name') is-invalid @enderror"
-                                           name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+                                           name="name"
+                                           value="{{ old('name') }}"
+                                           autocomplete="name"
+                                           autofocus
+                                           required>
 
                                     @error('name')
-                                        <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                     @enderror
@@ -34,7 +38,10 @@
                                 <div class="col-md-6">
                                     <input id="email" type="email"
                                            class="form-control @error('email') is-invalid @enderror"
-                                           name="email" value="{{ old('email') }}" autocomplete="email">
+                                           name="email"
+                                           value="{{ old('email') }}"
+                                           autocomplete="email"
+                                           required>
 
                                     @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -57,7 +64,10 @@
                                 <div class="col-md-6">
                                     <input id="username" type="text"
                                            class="form-control @error('username') is-invalid @enderror"
-                                           name="username" value="{{ old('username') }}" autocomplete="username">
+                                           name="username"
+                                           value="{{ old('username') }}"
+                                           autocomplete="username"
+                                           required>
 
                                     <div class="text-muted">
                                         <small>
@@ -85,18 +95,27 @@
 
                                 <div class="col-md-6">
                                     <div class="form-check">
-                                        <input name="gender" class="form-check-input @error('gender') is-invalid @enderror" id="male" type="radio"
-                                               value="male" {{ old('gender') == 'male' ? 'checked' : '' }}>
+                                        <input name="gender"
+                                               class="form-check-input @error('gender') is-invalid @enderror" id="male"
+                                               type="radio"
+                                               value="male" {{ old('gender') == 'male' ? 'checked' : '' }}
+                                               required>
                                         <label class="form-check-label" for="male">{{ __('Male') }}</label>
                                     </div>
                                     <div class="form-check">
-                                        <input name="gender" class="form-check-input @error('gender') is-invalid @enderror" id="female" type="radio"
-                                               value="female" {{ old('gender') == 'female' ? 'checked' : '' }}>
+                                        <input name="gender"
+                                               class="form-check-input @error('gender') is-invalid @enderror"
+                                               id="female" type="radio"
+                                               value="female" {{ old('gender') == 'female' ? 'checked' : '' }}
+                                               required>
                                         <label class="form-check-label" for="female">{{ __('Female') }}</label>
                                     </div>
                                     <div class="form-check">
-                                        <input name="gender" class="form-check-input @error('gender') is-invalid @enderror" id="others" type="radio"
-                                               value="others" {{ old('gender') == 'others' ? 'checked' : '' }}>
+                                        <input name="gender"
+                                               class="form-check-input @error('gender') is-invalid @enderror"
+                                               id="others" type="radio"
+                                               value="others" {{ old('gender') == 'others' ? 'checked' : '' }}
+                                               required>
                                         <label class="form-check-label" for="others">{{ __('Others') }}</label>
 
                                         @error('gender')
@@ -109,13 +128,16 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-right">
+                                    {{ __('Password') }}
+                                </label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                            class="form-control @error('password') is-invalid @enderror"
-                                           name="password" autocomplete="new-password">
+                                           name="password"
+                                           autocomplete="new-password"
+                                           required>
 
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -130,8 +152,12 @@
                                        class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
-                                           name="password_confirmation" autocomplete="new-password">
+                                    <input id="password-confirm"
+                                           type="password"
+                                           class="form-control"
+                                           name="password_confirmation"
+                                           autocomplete="new-password"
+                                           required>
                                 </div>
                             </div>
 
