323bd0e72baba26e411764d6b7005d20c73d8222
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-10-17 23:34:06 +0600

    feat: enter key submits the comment form and comment form design updated

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,18 +1,27 @@
-<div class="card-body {{ request()->routeIs('posts.index') ? ' pt-0' : 'bg-white rounded-top' }}">
+<div class="card-body {{ request()->routeIs('posts.index') ? 'pt-0' : 'bg-white rounded-top' }}">
     <form>
         <div class="form-group mb-3">
             <label class="form-control-label" for="comment-{{ $post->slug }}">
                 {{ __('Comments') }}
             </label>
-            <textarea class="form-control"
-                      id="comment-{{ $post->slug }}"
-                      name="comment"
-                      rows="3"
-                      resize="none"
-                      placeholder="Write your comment here..">{{ old('comment') }}</textarea>
+
+            <div class="media align-items-center">
+                <img alt="{{ auth()->user()->name }}"
+                     class="avatar avatar-sm rounded-circle mr-4"
+                     src="{{ auth()->user()->avatar }}">
+                <div class="media-body">
+                    <form>
+                        <textarea class="form-control commentTextarea"
+                                  id="comment-{{ $post->slug }}"
+                                  name="comment"
+                                  placeholder="{{ __('Write your comment') }}"
+                                  rows="1"
+                                  spellcheck="false"
+                                  data-post-slug="{{ $post->slug }}"
+                        >{{ old('comment') }}</textarea>
+                    </form>
+                </div>
+            </div>
         </div>
-        <button class="btn btn-primary btn-sm commentButton"
-                data-post-slug="{{ $post->slug }}"
-                data-request-is="{{ request()->routeIs('posts.index') }}">{{ __('Comment') }}</button>
     </form>
 </div>
