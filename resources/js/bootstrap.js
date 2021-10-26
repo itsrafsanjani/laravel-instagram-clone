5bcdb0e0fb1048b798318a30e6603e7a9432fc34
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-10-26 21:54:20 +0600

    feat: console.warn used in bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,9 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-} catch (e) {}
+} catch (e) {
+    console.warn(e.message)
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
