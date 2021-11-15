706d807c4aa99589f8f5457d7609646d875d4caa
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-11-15 21:52:46 +0600

    fix: _sidebar class fixed

--- resources/views/posts/_sidebar.blade.php
+++ resources/views/posts/_sidebar.blade.php
@@ -26,7 +26,7 @@
             </div>
         </div>
 
-        <div class="d-flex justify-content-lg-between justify-content-center align-items-center text-md-center px-4 py-2">
+        <div class="d-flex justify-content-md-between justify-content-around align-items-center text-md-center px-4 py-2">
             <div class="text-muted text-sm">
                 {{ __('Suggestions For You') }}
             </div>
