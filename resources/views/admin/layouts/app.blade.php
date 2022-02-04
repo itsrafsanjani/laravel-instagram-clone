4d5f89bdcf98ca1824fe9ebfdedf6c344e8d5a8e
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-02-04 15:56:00 +0600

    feat: datatables pagination icon added using datatables configuration

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -73,5 +73,21 @@
 <script src="{{ asset('backend/assets/js/argon.js?v=1.2.0') }}"></script>
 <!-- Custom JS-->
 @stack('scripts')
+
+<script>
+    (function ($, DataTable) {
+
+        // Datatable global configuration
+        $.extend(true, DataTable.defaults, {
+            "language": {
+                "paginate": {
+                    "previous": "<i class=\"fas fa-angle-left\"></i>",
+                    "next": "<i class=\"fas fa-angle-right\"></i>",
+                }
+            }
+        });
+
+    })(jQuery, jQuery.fn.dataTable);
+</script>
 </body>
 </html>
